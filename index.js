330105dfec5187e532b8bd605ed0fdd406924e58
Gustavo Brandão <brandaogimenez@gmail.com>
2021-03-30 05:40:14 -0400

    Create index.js
    Console.log

---
+++ index.js
@@ -0,0 +1,5 @@
+console.log("Gustavo 'Brandão'"); //String = Texto
+console.log('Gustavo "Brandão"');
+console.log(`'Gustavo' "Brandão"`);
+
+console.log(35, 15.85, 'Gustavo Brandão'); //Number = Número
